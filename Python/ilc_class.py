9667141e228c64833c82100064b7f2a46dcd5cda
Simon Valax <svalax@etel.ch>
2021-06-04 07:40:34 -0700

    implement Willson math, working quite nicelygit add --all!

--- Python/ilc_class.py
+++ Python/ilc_class.py
@@ -26,6 +26,8 @@ class Etel_ILC:
         
         self.learning_score = 0
         
+        # filter 
+        
         # python array data
         self.traj_theoPos_write = [] # array of double theo position
         self.traj_theoPos_read = []
@@ -34,6 +36,7 @@ class Etel_ILC:
         self.traj_tracking = [] 
         self.traj_time_stamp_read = []
         self.traj_currFfw_write = []
+        self.traj_currFfw_ILC_debug = []
         
         # c_double array data (for communication with RTX)
         self.traj_theoPos_write_rtx = [] # c_double array theo position to be pass to RTX
@@ -49,8 +52,10 @@ class Etel_ILC:
         self.L_filter = []
         self.axis_inverse_plant = []
                 
+        # load 
         self.load_traj()
         self.compute_acceleration_ffw()
+        self.load_L_filter_parameter('Lfilter/A.txt','Lfilter/B.txt','Lfilter/C.txt','Lfilter/D.txt')
         
         
         
@@ -65,9 +70,6 @@ class Etel_ILC:
         for n in range(len(c_double_input)):
             return_list[n] = c_double_input[n]
         return return_list
-        
-    def create_L_filter(self):
-        self.L_filter = signal.tf2zpk(self.b, self.a)
 
     def load_traj(self):
         df = pd.read_csv(self.trajectory_file_path)
@@ -85,15 +87,43 @@ class Etel_ILC:
     def compute_cps(self, data):
         N = len(data)
         T = self.sampling_time
-        yf = scipy.fftpack.fft(data)
+        yf = scipy.fft.fft(data)
         
         xf = np.linspace(0.0,1.0//(2.0*T), N//2)
         yf_abs = (2.0/N * np.abs(yf[:N//2]))**2
         return np.sum(yf_abs)
+    
+    def load_L_filter_parameter(self,A_path,B_path,C_path,D_path):
+        self.A = np.array(pd.read_csv(A_path,header=None,dtype=(float)).values)
+        self.B = np.array(pd.read_csv(B_path,header=None,dtype=(float)).values)
+        self.C = np.array(pd.read_csv(C_path,header=None,dtype=(float)).values)
+        self.D = np.array(pd.read_csv(D_path,header=None,dtype=(float)).values)
+        
+    # xk is internal state of the state space filter
+    # uk is the new state
+    def stateSpaceFilter(self,uk,xk):
+        xkp1= np.dot(self.A,xk) + np.dot(self.B,uk)
+        yk = np.dot(self.C,xk) + self.D*uk
+
+        return yk, xkp1
+    
+
+    def apply_L_filter(self,U):
+        # xk = internal state space filter value (np.shape(xk) = (len(B),1))
+        xk = np.zeros([len(self.B),1])
+        y = np.zeros(len(U))
+        
+        for kk, ui in enumerate(U):
+            y[kk], xk = self.stateSpaceFilter(ui, xk)        
+        return y
+    
+    def compute_new_ILC_ffw(self):
+        self.traj_currFfw_ILC_debug = self.traj_currFfw_write + np.multiply(self.apply_L_filter(self.traj_tracking),1e5)
 
         
     def compute_acceleration_ffw(self):
         self.traj_currFfw_write = np.multiply(self.traj_acc, (self.mass / self.kt)) 
+        #self.traj_currFfw_write = np.multiply(self.traj_acc, 0.0) 
         self.traj_currFfw_write_rtx = self.convert_to_c_double_type(self.traj_currFfw_write)
 
         
@@ -158,70 +188,4 @@ class Etel_ILC:
         plt.draw()
 
 
-toto = Etel_ILC('TheoTraj_for_ILC_1mmTrajVulcano1',12.6,25.7)
-
-# #load the dll
-# ilcDll = ctypes.WinDLL("C:\\ABR\\ILC\\ILC_WINDOWS_SIDE\\x64\\Debug\\ILC_DLL.dll");
-# CExecuteTrajectory = ilcDll.CExecuteTrajectory
-# CExecuteTrajectory.argtypes = POINTER(c_double), POINTER(c_double), POINTER(c_double), POINTER(c_double), POINTER(c_int)
-# CExecuteTrajectory.restype = c_int
-
-# #how long after theoretical mvt we want to monitor rtv values
-# samplingTime = 50e-6
-# nextraSecond = 0.2
-
-# #prepare the trajectory data
-# df = pd.read_csv('theoTrajLKT1mmVulc1_X.txt')
-# forward = convert_to_c_double_type(df['M0 in meter'].values)
-# ffwCurrentForward = convert_to_c_double_type(df['MF231 in A'].values)
-
-# nTotal = len(df['M0 in meter'].values)
-# timeStamps = (c_double * nTotal)()
-# 	
-# c_nTotal = c_int(nTotal)
-# ml1 = (c_double * nTotal)()
-
-# #prepare the plot window
-# timeStampsData = []
-# ml0Data = []
-# ml1Data = []
-
-# #line_ml0, line_ml1, axes = plot_iteration_results(timeStampsData, ml0Data, ml1Data)
-
-# #plt.show()
-# axes = plt.gca()
-
-# line_ml0, = axes.plot(timeStampsData, ml0Data, 'g+')
-# line_ml1, = axes.plot(timeStampsData, ml1Data, 'r+')
-# #maxY = 0;
-
-# nexecution = 2;
-# for i in range(0,nexecution*2):
-# 	print("forward: ",end = '')
-# 	print(i)
-# 	print(c_nTotal)
-
-# 	CExecuteTrajectory(timeStamps, forward, ffwCurrentForward, ml1, byref(c_nTotal))
-# 	for j in range(0,nTotal):
-# 		timeStampsData.append(timeStamps[j]*samplingTime)
-# 		ml0Data.append(forward[j])
-# 		ml1Data.append(ml1[j])
-
-# 	line_ml0.set_xdata(timeStampsData)
-# 	line_ml0.set_ydata(ml0Data)
-# 	line_ml1.set_xdata(timeStampsData)
-# 	line_ml1.set_ydata(ml1Data)
-# 		
-# 	axes.set_xlim(min(timeStampsData), max(timeStampsData)) #set the new axis limit
-# 	axes.set_ylim(min(ml1)-0.05*np.mean(ml1), max(ml1)+0.05*np.mean(ml1))
-# 	plt.draw()
-# 	plt.pause(1e-17)
-# 	time.sleep(0.1);
-# 	
-# plt.show();
-# print("Completed")
-
-# #plt.plot(forward, timerange);
-# #plt.plot(ml1, timerange);
-
-# 	
+toto = Etel_ILC('TheoTraj_for_ILC_1mmTrajVulcano1',0.0,25.7)
